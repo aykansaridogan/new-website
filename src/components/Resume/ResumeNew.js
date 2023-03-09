27ddd9a9f4e9aa65b7ad35f33e9b92ff53c8c755
Aykan SARIDOĞAN <105684427+aykansaridogan@users.noreply.github.com>
2023-03-09 18:14:45 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/aykansaridogan.pdf";
+import pdf from "../../Assets/aykansaridogan.pdf";
+import Footer from "../Footer.js";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://www.docfly.com/files/0c7vi6hn0pqi-6e7c32ee";
+pdf;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
