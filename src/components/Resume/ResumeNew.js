7936a33365aa261443930469a88fad06e0ff53d2
Aykan SARIDOĞAN <105684427+aykansaridogan@users.noreply.github.com>
2023-03-09 18:18:12 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,6 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/aykansaridogan.pdf";
-import Footer from "../Footer.js";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
